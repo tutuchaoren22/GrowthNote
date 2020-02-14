c1f780c78f35a0ce1cd3724e860aefa889c31836
duyuqian <15738381799@163.com>
2020-02-14 14:36:49 +0800

    [duyuqian] feat: create the UserRepository class

--- src/main/java/com/example/GrowthNote/GrowthNoteApplication.java
+++ src/main/java/com/example/GrowthNote/GrowthNoteApplication.java
@@ -1,14 +1,22 @@
 package com.example.GrowthNote;
 
+import com.example.GrowthNote.repositories.UserRepository;
 import org.springframework.boot.SpringApplication;
+
 //import org.springframework.boot.autoconfigure.SpringBootApplication;
 //
 //@SpringBootApplication
 public class GrowthNoteApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 //		SpringApplication.run(GrowthNoteApplication.class, args);
-		System.out.println("hello");
-	}
+
+		//----------test UserRepository----------
+//		UserRepository userRepository=new UserRepository();
+//        String name = "xiaoming";
+//        String password = "b3275960d68fda9d831facc0426c3bbc";
+//        System.out.println(userRepository.getUserByNameAndPassword(name, password));
+
+    }
 
 }
--- src/main/java/com/example/GrowthNote/entities/User.java
+++ src/main/java/com/example/GrowthNote/entities/User.java
@@ -13,5 +13,17 @@ public class User {
         this.name = name;
         this.password = password;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
 
