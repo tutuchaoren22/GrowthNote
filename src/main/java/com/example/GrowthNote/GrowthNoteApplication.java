176c11e7dbed6932b1e3167b3aa265d00316bfa9
duyuqian <15738381799@163.com>
2020-02-14 15:05:46 +0800

    [duyuqian] feat: create the Application class

--- src/main/java/com/example/GrowthNote/GrowthNoteApplication.java
+++ src/main/java/com/example/GrowthNote/GrowthNoteApplication.java
@@ -1,7 +1,10 @@
 package com.example.GrowthNote;
 
+import com.example.GrowthNote.controllers.UserController;
 import com.example.GrowthNote.repositories.UserRepository;
 import org.springframework.boot.SpringApplication;
+
+import java.util.Scanner;
 
 //import org.springframework.boot.autoconfigure.SpringBootApplication;
 //
@@ -11,12 +14,23 @@ public class GrowthNoteApplication {
     public static void main(String[] args) {
 //		SpringApplication.run(GrowthNoteApplication.class, args);
 
-		//----------test UserRepository----------
+        //----------test UserRepository----------
 //		UserRepository userRepository=new UserRepository();
 //        String name = "xiaoming";
 //        String password = "b3275960d68fda9d831facc0426c3bbc";
 //        System.out.println(userRepository.getUserByNameAndPassword(name, password));
 
+        UserController userController = new UserController();
+        Boolean isPassIn = false;
+        while (!isPassIn){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("请输入用户名：");
+            String name = scanner.next();
+            System.out.println("请输入密码：");
+            String password = scanner.next();
+            isPassIn = userController.getUserByNameAndPassword(name, password);
+            System.out.println("has user passed in:"+isPassIn);
+        }
+        System.out.println("进入主页");
     }
-
 }
